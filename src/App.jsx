a1b8d38ce14cbe32dd5509d40ea3170a5f6b129c
RuchirGupta-EVS <ruchir.gupta@evssolution.com>
2023-02-20 17:51:00 +0530

    ui changes for image generator input field started

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import ImageGenerator from "./Components/Image Generator Engine/ImageGenerator";
+
+
+function App() {
+  return (
+    <>
+        <ImageGenerator/>
+    </>
+  )
+}
+
+export default App;
