9d5254cc452c35f440525761f9f115bffc95f586
RuchirGupta-EVS <ruchir.gupta@evssolution.com>
2023-02-21 13:05:08 +0530

    search ui created without css

--- src/Components/Image Generator Engine/index.jsx
+++ src/Components/Image Generator Engine/index.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
 
 const ImageGenerator = () => {
   return (
-    <div>Hello This is AI</div>
-  )
-}
+    <>
+      <div>
+        <div class="mb-3 container">
+          <label for="exampleFormControlInput1" class="form-label">
+            Enter your Imagination Below:
+          </label>
+          <input
+            type="text"
+            class="form-control"
+            id="exampleFormControlInput1"
+            placeholder="/imagine"
+          />
+        </div>
+        <div class="form-check form-check-inline">
+          <input
+            class="form-check-input"
+            type="checkbox"
+            id="inlineCheckbox1"
+            value="option1"
+          />
+          <label class="form-check-label" for="inlineCheckbox1">
+            Search Through Web
+          </label>
+        </div>
+        <div class="form-check form-check-inline">
+          <input
+            class="form-check-input"
+            type="checkbox"
+            id="inlineCheckbox2"
+            value="option2"
+          />
+          <label class="form-check-label" for="inlineCheckbox2">
+            Search Through AI Server
+          </label>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default ImageGenerator;
